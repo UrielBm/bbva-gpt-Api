486229905aea1fd9d79b1774ad1f34597a12ebcc
UrielBm <urielbenitez96@hotmail.com>
2024-03-25 01:45:44 -0600

    hotfix:port

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({origin: "*",
@@ -8,6 +9,6 @@ async function bootstrap() {
   preflightContinue: false,
   optionsSuccessStatus: 204
 });
-  await app.listen(3000);
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
