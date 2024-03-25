b3fffeff9a828ba11fdf62354835d65dcbe3f29e
UrielBm <urielbenitez96@hotmail.com>
2024-03-25 01:51:55 -0600

    hotfix: front-origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: "*",
+  app.enableCors({origin: "https://bbva-front.vercel.app",
   methods: "GET,POST,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   optionsSuccessStatus: 204
