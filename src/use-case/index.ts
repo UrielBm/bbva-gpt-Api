9421efd80387fea7c9caf56dcb8a471038832184
UrielBm <urielbenitez96@hotmail.com>
2024-03-25 00:59:25 -0600

    hotfix: path into index.ts

--- src/use-case/index.ts
+++ src/use-case/index.ts
@@ -1,5 +1,5 @@
 export * from "./createThreadUseCase"
 export * from "./createMessageUseCase"
-export * from "./CreateRunUseCase"
+export * from "./createRunUseCase"
 export * from "./checkStatusUseCase"
 export * from "./getMessagesListUseCase"
