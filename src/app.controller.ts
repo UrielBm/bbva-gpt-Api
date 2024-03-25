ee26e0e737dda4773ec3b6973990fee3768f64e5
UrielBm <urielbenitez96@hotmail.com>
2024-03-25 01:31:11 -0600

    feature: inicio de api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { QuestionDto } from './dtos/question.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+@Get("/")
+handleWelcom(){
+  return {welcome: "la api esta funcionando"}
+}
 @Post("create-thread")
 async handleCreateThread(){
  return this.appService.handleCreateThread()
